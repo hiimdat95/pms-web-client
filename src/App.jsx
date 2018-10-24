c48827ccbdc47ba6b93703bc7241412909cff788
LTienDat <k0olano0.ltd@gmail.com>
2018-10-24 17:45:18 +0700

    commit app.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,3 +4,21 @@ import PropTypes from 'prop-types';
 import configureStore from './store/configureStore';
 import routes from './routes';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+const store = configureStore();
+
+
+class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+            </Provider>
+        );
+    }
+}
+
+// App.propTypes = {
+//   store: PropTypes.object.isRequired,
+//   history: PropTypes.object.isRequired
+// }
+
+export default App;
